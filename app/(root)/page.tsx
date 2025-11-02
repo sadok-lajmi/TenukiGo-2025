a15debe427551474374198844570c5970289f652
Saad Khazari <saadeddine.khazari@gmail.com>
2025-11-02 13:00:06 +0100

    Squashed 'frontend/' content from commit 4beb3b1
    git-subtree-dir: frontend
    git-subtree-split: 4beb3b14f98c1922851821eccde76560296c4bc2

---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+import Header from '@/components/Header'
+import VideoCard from '@/components/VideoCard'
+import { dummyCards } from '@/constants'
+
+const Page = () => {
+  return (
+    <main className='wrapper page'>
+      <Header title='All Videos' subHeader='Public Library' />
+
+
+      <section className='video-grid'>
+        {dummyCards.map((card) => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
+    </main>
+  )
+}
+
+export default Page
