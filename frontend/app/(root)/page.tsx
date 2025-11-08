7da5d4a372424929a1cfe54118ab7d87a0e8e7f8
sadok <sadok.lajmi@imt-atlantique.net>
2025-11-08 16:14:56 +0100

    modification de la page d'acceuil pour fetch et afficher les videos

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,17 +1,32 @@
 import Header from '@/components/Header'
 import VideoCard from '@/components/VideoCard'
-import { dummyCards } from '@/constants'
+import {useEffect, useState} from 'react'
+
 
 const Page = () => {
+
+  const [videos, setVideos] = useState<Array<VideoCardProps>>([])
+
+  useEffect(() => {
+    // Fetch videos from API
+    const fetchVideos = async () => {
+      const response = await fetch(`${process.env.API_URL}/videos`)
+      const data = await response.json()
+      setVideos(data) }
+    fetchVideos()
+  }, [])
+
   return (
     <main className='wrapper page'>
       <Header title='All Videos' subHeader='Public Library' />
-
-
       <section className='video-grid'>
-        {dummyCards.map((card) => (
-          <VideoCard key={card.id} {...card} />
-        ))}
+        {videos.length === 0 ? (
+          <p>No videos found.</p>
+        ) : (
+          videos.map((video) => (
+            <VideoCard key={video.id} {...video} />
+          ))
+        )}
       </section>
     </main>
   )
