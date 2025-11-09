545dbc3b89afe9755cb9ecbf09ed54e4a4c1198c
sadok <sadok.lajmi@imt-atlantique.net>
2025-11-09 18:42:14 +0100

    corrections à videocard

--- frontend/components/VideoCard.tsx
+++ frontend/components/VideoCard.tsx
@@ -7,9 +7,9 @@ import React from 'react'
 const VideoCard = ({ id, title, thumbnail="/assets/samples/thumbnail (1).png", createdAt, duration }: VideoCardProps) => {
   return (
     <Link href={`/video/${id}`} className='video-card'>
-        <Image src={thumbnail} alt='thumbnail' width={290} height={160} className='thumbnail' />
+        <Image src={thumbnail ?? "/assets/samples/thumbnail (1).png"} alt='thumbnail' width={290} height={160} className='thumbnail' />
         <article>
-            <h2>{title} - {" "} {createdAt.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</h2>
+            <h2>{title} - {" "} {createdAt ? createdAt.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }) : 'Unknown date'}</h2>
         </article>
         <button onClick={() => {}} className='copy-btn'>
             <Image src='/assets/icons/link.svg' alt='copy' width={18} height={18} />
