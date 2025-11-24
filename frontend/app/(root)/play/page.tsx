3f641f318fc5c5236c10853800ef19e43131e90f
Saad Khazari <saadeddine.khazari@gmail.com>
2025-11-24 14:53:51 +0100

    Ajout d'un plateau pour jouer

---
+++ frontend/app/(root)/play/page.tsx
@@ -0,0 +1,10 @@
+import GoPlayable from "@/components/go/GoPlayable";
+
+
+export default function PlayPage() {
+    return (
+        <div className="min-h-screen neutral-50 py-10">
+            <GoPlayable />
+        </div>
+    );
+}
