0a1522284d5aca9697995ce880b0d739a0ef921e
sadok <sadok.lajmi@imt-atlantique.net>
2025-11-24 21:51:50 +0100

    amélioration des pages en relation avec le livestream

--- frontend/app/(root)/stream/page.tsx
+++ frontend/app/(root)/stream/page.tsx
@@ -2,8 +2,10 @@
 import FormField from "@/components/FormField";
 import VideoPlayer from "@/components/VideoPlayer";
 import { ChangeEvent, useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const [password, setPassword] = useState("");
 
   const [formData, setFormData] = useState({
     title: '',
@@ -42,6 +44,50 @@ const Page = () => {
     setStreamUrl(e.target.value);
   };
 
+  // handle submit
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  const handleSubmit = async (e: React.FormEvent) => {  
+    e.preventDefault();
+    // Validate password
+    if (password !== process.env.NEXT_PUBLIC_PASSWORD) {
+        setError("Mot de passe incorrect.");
+        return;
+    }
+    
+    if (!streamUrl) {
+      alert("Please enter a stream URL.");
+      return;
+    }
+    if (!formData.title || !formData.player_b || !formData.player_w) {
+      alert("Please fill in all required fields (*).");
+      return;
+    }
+    const dataToSend = new FormData();
+    dataToSend.append('title', formData.title);
+    dataToSend.append('style', formData.style);
+    dataToSend.append('black', formData.player_b);
+    dataToSend.append('white', formData.player_w);
+    dataToSend.append('description', formData.description);
+    dataToSend.append('url', streamUrl);
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/start_stream`, {
+            method: 'POST',
+            body: dataToSend,
+        });
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        const error = responseData['error'] || 'An error occurred during upload.';
+        setError(error.message || 'An error occurred during upload.');
+        return;
+    }
+    // On success, redirect to the livestream watch page
+    const streamId = responseData['stream_id'];
+    router.push(`/watch/${streamId}`);
+        
+  };
+
   return (
     <div className='wrapper-md stream-page'>
       <h1>Live Stream</h1>
@@ -49,7 +95,7 @@ const Page = () => {
         <h1 className='text-2xl font-semibold'>Match info</h1>
         <FormField 
           id='title'
-          label='Stream Title'
+          label='Stream Title *'
           value={formData.title}
           onChange={handleInputChange}
           placeholder='Enter the title of your stream'
@@ -69,7 +115,7 @@ const Page = () => {
         />
         <FormField 
           id='player_b'
-          label='Player for Black'
+          label='Player for Black *'
           as = 'search'
           options={players}
           value={formData.player_b}
@@ -78,7 +124,7 @@ const Page = () => {
         />
         <FormField 
           id='player_w'
-          label='Player for White'
+          label='Player for White *'
           as ='search'
           options={players}
           value={formData.player_w}
@@ -106,10 +152,18 @@ const Page = () => {
         />
         <VideoPlayer url={streamUrl ? streamUrl : "http://localhost:8080/live/streamkey/index.m3u8"} />
       </form>
-      <div className="flex justify-center">    
-      <button type='submit' className="bg-blue-400 text-white px-4 py-2 rounded-lg w-32">
-          Start
-      </button>
+      {error && <p className="text-red-500 mt-2">{error}</p>}
+      <div className="flex justify-center mt-4 gap-2">   
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Entrez le mot de passe"
+          className="border border-gray-300 rounded px-3 py-2 mt-2 w-30"
+         /> 
+        <button type='submit' className="bg-blue-400 text-white px-4 py-2 rounded-lg w-30" onClick={handleSubmit}>
+            Lancer
+        </button>
       </div>
     </div>
   )
