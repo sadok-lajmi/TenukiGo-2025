036e729bda33512ffe61fb8df12ba2f4241111e4
Antonin <antonin.polette@imt-atlantique.net>
2025-11-28 00:01:27 +0100

    Modif directories

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -2,21 +2,27 @@ from pathlib import Path
 import os
 
 # -------------------------------
+# API CONFIG
+# -------------------------------
+HOST = "0.0.0.0"
+PORT = 8000
+
+# -------------------------------
 # BASE DIRECTORIES
 # -------------------------------
 BASE_DIR = Path(__file__).parent.parent.resolve()
 
 # Upload directories
-UPLOAD_DIR = BASE_DIR / "uploads"
-VIDEO_DIR = UPLOAD_DIR / "videos"
-THUMBNAIL_DIR = UPLOAD_DIR / "thumbnails"
-SGF_DIR = UPLOAD_DIR / "sgf"
+LOCAL_UPLOAD_DIR = BASE_DIR / "uploads"
+VIDEO_DIR = LOCAL_UPLOAD_DIR / "videos"
+THUMBNAIL_DIR = LOCAL_UPLOAD_DIR / "thumbnails"
+SGF_DIR = LOCAL_UPLOAD_DIR / "sgf"
+UPLOAD_DIR = "app/uploads" # Corresponds to Docker container path
 
 # Create directories if they don't exist
 VIDEO_DIR.mkdir(parents=True, exist_ok=True)
 THUMBNAIL_DIR.mkdir(parents=True, exist_ok=True)
 SGF_DIR.mkdir(parents=True, exist_ok=True)
-
 
 # -------------------------------
 # DATABASE CONFIG
@@ -30,7 +36,6 @@ DB_URL = os.getenv("DB_URL")
 CLUB_PASSWORD = os.getenv("CLUB_PASSWORD")
 
 # -------------------------------
-# API CONFIG
+# ANALYSE MODULE CONFIG
 # -------------------------------
-HOST = "0.0.0.0"
-PORT = 8000
+ANALYSE_SERVICE_URL = "http://localhost:5000"
