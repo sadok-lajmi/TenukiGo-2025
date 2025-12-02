9f461698631cbc3da6219a38fdd71e09aea5de7a
Antonin <antonin.polette@imt-atlantique.net>
2025-12-02 21:21:27 +0100

    Modif allow_origins for dev

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -22,10 +22,7 @@ app = FastAPI(title="Go Game API")
 # ======================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  
     allow_headers=["*"],
